2ae988ce69851030648f75af9558b23d34f8a248
Goran Mahovlic <goran.mahovlic@gmail.com>
2015-03-05 21:35:54 +0100

    Update Thingspeak.py

--- outputs/Thingspeak.py
+++ outputs/Thingspeak.py
@@ -12,19 +12,19 @@ class Print(output.Output):
 		self.APIKey=data["APIKey"]
 #		pass
 	def outputData(self,dataPoints):
-    	Data[]
-	print ""
+    		Data[]
+	#print ""
 	#print "Time: " + str(datetime.datetime.now())
-	for i in dataPoints:
-      		Data[i] = str(i["value"])
-	#	print i["name"] + ": " + str(i["value"]) + " " + i["symbol"]
-		print Data[i]
+		for i in dataPoints:
+      			Data[i] = str(i["value"])
+	#		print i["name"] + ": " + str(i["value"]) + " " + i["symbol"]
+			print Data[i]
 		return True
-    	params = urllib.urlencode({'field1': Data[0], 'field2': Data[1],'field3': Data[2],'field4': Data[3], 'field5': Data[4],'field6': Data[5],'field7': Data[6],'field8': Data[7],'key':self.APIKey})
-    	headers = {"Content-type": "application/x-www-form-urlencoded","Accept":"text/plain"}
-    	conn = httplib.HTTPConnection("api.thingspeak.com:80")
-    	conn.request("POST", "/update", params, headers)
-    	response = conn.getresponse()
-    	print response.status, response.reason
-    	data = response.read()
-    	conn.close()
+    		params = urllib.urlencode({'field1': Data[0], 'field2': Data[1],'field3': Data[2],'field4': Data[3], 'field5': Data[4],'field6': Data[5],'field7': Data[6],'field8': Data[7],'key':self.APIKey})
+    		headers = {"Content-type": "application/x-www-form-urlencoded","Accept":"text/plain"}
+    		conn = httplib.HTTPConnection("api.thingspeak.com:80")
+    		conn.request("POST", "/update", params, headers)
+    		response = conn.getresponse()
+    		print response.status, response.reason
+    		data = response.read()
+    		conn.close()
